71304c0c7679c4f3eb4699690818d1f90dd2178b
Yoann Agnes <agnes.yoann@gmail.com>
2023-09-14 16:41:33 +0200

    Refonte complete
    Full JS,
    Responsive a finir, quelque fonctionnalité a ajouter.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,144 @@
-const cards = document.querySelectorAll(".memory-card");
+// ----- Variables globales -----
+
+const equipes = {
+  canada: "/assets/img/rugbyCdm/canada.png",
+  chili: "/assets/img/rugbyCdm/chile.png",
+  angleterre: "/assets/img/rugbyCdm/england.png",
+  fidji: "/assets/img/rugbyCdm/fijans.png",
+  france: "/assets/img/rugbyCdm/french.png",
+  georgie: "/assets/img/rugbyCdm/georgia.png",
+  irlande: "/assets/img/rugbyCdm/irish.png",
+  italie: "/assets/img/rugbyCdm/italie.png",
+  coteIvoire: "/assets/img/rugbyCdm/ivory.png",
+  japon: "/assets/img/rugbyCdm/japan.png",
+  lakapi: "/assets/img/rugbyCdm/lakapi.png",
+  argentine: "/assets/img/rugbyCdm/losPumas.png",
+  namibia: "/assets/img/rugbyCdm/namibia.png",
+  nouvelleZelande: "/assets/img/rugbyCdm/newZealand.png",
+  portugal: "/assets/img/rugbyCdm/portuguese.png",
+  roumanie: "/assets/img/rugbyCdm/romania.png",
+  russie: "/assets/img/rugbyCdm/russia.png",
+  ecosse: "/assets/img/rugbyCdm/scottish.png",
+  espagne: "/assets/img/rugbyCdm/spain.png",
+  springbok: "/assets/img/rugbyCdm/springbok.png",
+  tonga: "/assets/img/rugbyCdm/tonga.png",
+  uruguay: "/assets/img/rugbyCdm/uruguay.png",
+  usa: "/assets/img/rugbyCdm/usa.png",
+  wales: "/assets/img/rugbyCdm/wales.png",
+  australie: "/assets/img/rugbyCdm/wallabies.png",
+  zimbabwe: "/assets/img/rugbyCdm/zimbabwe.png",
+};
+
+let cards = document.querySelectorAll(".memory-card");
+let gameLogos = {};
+
+// ----- Fonctions utilitaires -----
+
+function previewCards(difficulty) {
+  const previewTimes = {
+    easy: 2000,
+    medium: 1500,
+    hard: 1000,
+    impossible: 300,
+  };
+
+  const previewDuration = previewTimes[difficulty];
+
+  cards.forEach((card) => card.classList.add("flip"));
+  setTimeout(() => {
+    cards.forEach((card) => card.classList.remove("flip"));
+    lockBoard = false; // Permet aux joueurs de cliquer sur les cartes après la prévisualisation
+  }, previewDuration);
+}
+
+function duplicateLogos(logos) {
+  let duplicated = {};
+  for (let key in logos) {
+    duplicated[key + "_1"] = logos[key];
+    duplicated[key + "_2"] = logos[key];
+  }
+  return duplicated;
+}
+
+function getRandomKeys(obj, num) {
+  let shuffledKeys = Object.keys(obj);
+  let result = [];
+  while (result.length < num) {
+    let randomIndex = Math.floor(Math.random() * shuffledKeys.length);
+    result.push(shuffledKeys.splice(randomIndex, 1)[0]);
+  }
+  return result;
+}
+
+function shuffleAssoc(obj) {
+  const entries = Object.entries(obj);
+  for (let i = entries.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [entries[i], entries[j]] = [entries[j], entries[i]];
+  }
+  return Object.fromEntries(entries);
+}
+
+function generateGameLogos(difficulty) {
+  let numTeams;
+  switch (difficulty) {
+    case "easy":
+      numTeams = 6;
+      break;
+    case "medium":
+      numTeams = 8;
+      break;
+    case "hard":
+      numTeams = 10;
+      break;
+    case "impossible":
+      numTeams = 10;
+      break;
+  }
+
+  const selectedTeamsKeys = getRandomKeys(equipes, numTeams);
+  gameLogos = {};
+  selectedTeamsKeys.forEach((key) => {
+    gameLogos[key] = equipes[key];
+  });
+
+  gameLogos = duplicateLogos(gameLogos);
+  gameLogos = shuffleAssoc(gameLogos);
+}
+
+function generateCards(difficulty) {
+  const cardsContainer = document.getElementById("cardsContainer");
+  cardsContainer.innerHTML = ""; // Réinitialise le conteneur des cartes seulement
+
+  Object.entries(gameLogos).forEach(([team, logo]) => {
+    const card = document.createElement("div");
+    card.className = "memory-card";
+    card.dataset.framework = team;
+
+    const frontFace = document.createElement("img");
+    frontFace.className = "front-face";
+    frontFace.src = logo;
+
+    const backFace = document.createElement("img");
+    backFace.className = "back-face";
+    backFace.src = "/assets/img/rugbyCdm/backgroundCard.png";
+
+    card.appendChild(frontFace);
+    card.appendChild(backFace);
+    cardsContainer.appendChild(card);
+  });
+
+  cards = document.querySelectorAll(".memory-card");
+
+  cards.forEach((card) => card.addEventListener("click", flipCard));
+
+  console.log("Nombre de cartes générées:", cards.length);
+
+  // Prévisualisation des cartes pour le joueur
+  previewCards(difficulty);
+}
+
+// ----- Fonctions de gestion du jeu -----
 
 let hasFlippedCard = false;
 let lockBoard = false;
@@ -8,88 +148,64 @@ let matchedCount = 0;
 let timer;
 let seconds = 0;
 
-function previewCards() {
-  cards.forEach((card) => card.classList.add("flip"));
-
-  // Cachez les cartes après 2 secondes et démarrez le chrono
-  setTimeout(() => {
-    cards.forEach((card) => card.classList.remove("flip"));
-    startTimer(); // Démarrer le chrono après la prévisualisation
-  }, 2000);
-}
-
-// Appel de la fonction preview au chargement de la page
-window.onload = () => {
-  shuffle();
-  previewCards();
-};
-
-function startTimer() {
-  if (timer) {
-    stopTimer(); // Si le timer est déjà en cours, l'arrêter
-  }
-  timer = setInterval(function () {
-    seconds++;
-    document.getElementById("timer").textContent = `Temps écoulé : ${seconds}s`;
-  }, 1000);
-}
-
-function stopTimer() {
-  clearInterval(timer);
-}
-
-function incrementCounter() {
-  moveCount++;
+function updateMoveCounter() {
   document.getElementById(
     "moveCounter"
   ).textContent = `Mouvements : ${moveCount}`;
 }
 
-function flipCard() {
-  if (lockBoard) return;
-  if (this === firstCard) return;
+function updateTimer() {
+  document.getElementById("timer").textContent = `Temps écoulé : ${seconds}s`;
+}
 
+function startTimer() {
+  if (timer) {
+    clearInterval(timer);
+  }
+  timer = setInterval(() => {
+    seconds++;
+    updateTimer();
+  }, 1000);
+}
+
+// ----- Fonctions de gestion du jeu -----
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+function flipCard() {
+  if (lockBoard || this === firstCard) return;
   this.classList.add("flip");
 
   if (!hasFlippedCard) {
     if (moveCount === 0) startTimer();
-
     hasFlippedCard = true;
     firstCard = this;
     return;
   }
 
   secondCard = this;
-  incrementCounter();
+  moveCount++;
+  updateMoveCounter();
   checkForMatch();
 }
 
 function checkForMatch() {
-  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-  isMatch ? disableCards() : unflipCards();
-}
-
-function disableCards() {
-  firstCard.removeEventListener("click", flipCard);
-  secondCard.removeEventListener("click", flipCard);
-  firstCard.classList.add("matched");
-  secondCard.classList.add("matched");
-
-  matchedCount += 2;
-  if (matchedCount === cards.length) {
-    stopTimer();
-
-    document.getElementById(
-      "finalMoveCounter"
-    ).textContent = `Mouvements : ${moveCount}`;
-    document.getElementById(
-      "finalTimer"
-    ).textContent = `Temps écoulé : ${seconds}s`;
-
-    document.getElementById("congratsModal").style.display = "block";
+  if (
+    firstCard.classList.contains("death-card") ||
+    secondCard.classList.contains("death-card")
+  ) {
+    endGame();
+    return;
   }
+  // Ignore les suffixes _1 et _2 lors de la comparaison
+  let firstCardKey = firstCard.dataset.framework.replace(/(_1|_2)$/, "");
+  let secondCardKey = secondCard.dataset.framework.replace(/(_1|_2)$/, "");
 
-  resetBoard();
+  let isMatch = firstCardKey === secondCardKey;
+  isMatch ? disableCards() : unflipCards();
 }
 
 function unflipCards() {
@@ -101,42 +217,96 @@ function unflipCards() {
   }, 1500);
 }
 
-function resetBoard() {
-  [hasFlippedCard, lockBoard] = [false, false];
-  [firstCard, secondCard] = [null, null];
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+  firstCard.classList.add("matched");
+  secondCard.classList.add("matched");
+  matchedCount += 2;
+
+  // Utiliser le tableau cards mis à jour
+  if (matchedCount === cards.length) {
+    clearInterval(timer);
+    showCongratsModal();
+  }
+
+  resetBoard();
 }
 
-function shuffle() {
-  cards.forEach((card) => {
-    let randomPos = Math.floor(Math.random() * cards.length);
-    card.style.order = randomPos;
-  });
+function startGame(difficulty) {
+  generateGameLogos(difficulty);
+  generateCards(difficulty);
+  hideModals();
 }
 
-cards.forEach((card) => card.addEventListener("click", flipCard));
+function endGame() {
+  clearInterval(timer);
+  alert("Vous avez cliqué sur la carte de la mort. Vous avez perdu !");
+  resetGame();
+}
 
-document.getElementById("resetButton").addEventListener("click", function () {
-  // Cachez le modal
-  document.getElementById("congratsModal").style.display = "none";
-
+function resetGame() {
+  lockBoard = true;
+  hideModals();
+  document.getElementById("difficultyModal").style.display = "block";
   moveCount = 0;
   matchedCount = 0;
   seconds = 0;
-  document.getElementById(
-    "moveCounter"
-  ).textContent = `Mouvements : ${moveCount}`;
-  document.getElementById("timer").textContent = `Temps écoulé : ${seconds}s`;
-
-  shuffle();
-
+  updateMoveCounter();
+  updateTimer();
+  generateCards();
   cards.forEach((card) => {
     card.classList.remove("flip");
     card.classList.remove("matched");
     card.addEventListener("click", flipCard);
   });
+  // previewCards();  // À décommenter si nécessaire
+}
 
-  // Prévisualisez les cartes avant de démarrer le jeu
-  previewCards();
+// ----- Gestion des modals -----
+
+function showDifficultyModal() {
+  // document.getElementById("modalBackdrop").style.display = "block";
+  document.getElementById("congratsModal").style.display = "none";
+  document.getElementById("difficultyModal").style.display = "block";
+  lockBoard = true;
+}
+
+function hideModals() {
+  // document.getElementById("modalBackdrop").style.display = "none";
+  document.getElementById("congratsModal").style.display = "none";
+  document.getElementById("difficultyModal").style.display = "none";
+  lockBoard = false;
+}
+
+function showCongratsModal() {
+  document.getElementById(
+    "finalMoveCounter"
+  ).textContent = `Mouvements : ${moveCount}`;
+  document.getElementById(
+    "finalTimer"
+  ).textContent = `Temps écoulé : ${seconds}s`;
+
+  // document.getElementById("modalBackdrop").style.display = "block";
+  document.getElementById("congratsModal").style.display = "block";
+  document.getElementById("difficultyModal").style.display = "none";
+  lockBoard = true;
+}
+
+// ----- Initialisation -----
+
+// Appel de la fonction shuffle au chargement de la page
+window.onload = () => {
+  resetGame();
+};
+
+// Initialisation des écouteurs d'événements
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
+document.getElementById("resetButton").addEventListener("click", resetGame);
+document.querySelectorAll(".difficulty").forEach((button) => {
+  button.addEventListener("click", function () {
+    let difficulty = this.getAttribute("data-difficulty");
+    startGame(difficulty);
+  });
 });
-
-shuffle();
--- memoryGame.php
+++ memoryGame.php
@@ -1,124 +1,45 @@
-<?php
-
-// Définition du tableau associatif contenant les noms des équipes et leurs logos respectifs
-$equipes = array(
-    "Canada" => "/assets/img/rugbyCdm/canada.png",
-    "Chili" => "/assets/img/rugbyCdm/chile.png",
-    "Angleterre" => "/assets/img/rugbyCdm/england.png",
-    "Fidji" => "/assets/img/rugbyCdm/fijans.png",
-    "France" => "/assets/img/rugbyCdm/french.png",
-    "Georgie" => "/assets/img/rugbyCdm/georgia.png",
-    "Irlande" => "/assets/img/rugbyCdm/irish.png",
-    "Italie" => "/assets/img/rugbyCdm/italie.png",
-    "Côte d'Ivoire" => "/assets/img/rugbyCdm/ivory.png",
-    "Japon" => "/assets/img/rugbyCdm/japan.png",
-    "Lakapi" => "/assets/img/rugbyCdm/lakapi.png",
-    "Argentine" => "/assets/img/rugbyCdm/losPumas.png",
-    "Namibia" => "/assets/img/rugbyCdm/namibia.png",
-    "Nouvelle-Zelande" => "/assets/img/rugbyCdm/newZealand.png",
-    "Portugais" => "/assets/img/rugbyCdm/portuguese.png",
-    "Roumanie" => "/assets/img/rugbyCdm/romania.png",
-    "Russie" => "/assets/img/rugbyCdm/russia.png",
-    "Ecosse" => "/assets/img/rugbyCdm/scottish.png",
-    "Espagne" => "/assets/img/rugbyCdm/spain.png",
-    "Springbok" => "/assets/img/rugbyCdm/springbok.png",
-    "Tonga" => "/assets/img/rugbyCdm/tonga.png",
-    "Uruguay" => "/assets/img/rugbyCdm/uruguay.png",
-    "US" => "/assets/img/rugbyCdm/usa.png",
-    "Wales" => "/assets/img/rugbyCdm/wales.png",
-    "Australie" => "/assets/img/rugbyCdm/wallabies.png",
-    "Zimbabwe" => "/assets/img/rugbyCdm/zimbabwe.png",
-
-);
-
-// Sélectionne 8 équipes aléatoirement parmi toutes les équipes disponibles
-$selected_teams_keys = array_rand($equipes, 8);
-$selected_logos = [];
-foreach ($selected_teams_keys as $key) {
-    $selected_logos[$key] = $equipes[$key];
-}
-
-// Duplication des logos pour avoir 2 cartes identiques pour chaque équipe (nécessaire pour le jeu de mémoire)
-$game_logos = array_merge($selected_logos, $selected_logos);
-
-// Mélange aléatoire des équipes pour l'affichage dans le jeu
-shuffle_assoc($game_logos);
-
-// Fonction pour mélanger un tableau associatif tout en conservant les clés
-function shuffle_assoc(&$array)
-{
-    $keys = array_keys($array);
-    shuffle($keys);
-    $new = [];
-    foreach ($keys as $key) {
-        $new[$key] = $array[$key];
-    }
-    $array = $new;
-    return true;
-}
-?>
-
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Memory Game</title>
-    <link rel="stylesheet" href="/assets/style/style.css" />
+    <link rel="stylesheet" href="/assets/style/style.css">
 </head>
+
 <body>
     <header>
-
-    <!-- Afficheur du nombre de mouvements effectués par le joueur -->
-        <p id="moveCounter">Mouvements : 0</p>
-
-        <!-- Afficheur du temps écoulé depuis le début du jeu -->
-        <p id="timer">Temps écoulé : 0s</p>
+        <h1>Memory Game Rugby</h1>
+        <div>
+            <p id="moveCounter">Mouvements : 0</p>
+            <p id="timer">Temps écoulé : 0s</p>
+        </div>
     </header>
 
     <main id="gameContainer">
-            <!-- Modale de sélection de la difficulté -->
-    <div id="difficultyModal" class="modal">
-        <h2>Choisissez votre difficulté</h2>
-        <button class="difficulty" data-difficulty="easy">Facile</button>
-        <button class="difficulty" data-difficulty="medium">Moyen</button>
-        <button class="difficulty" data-difficulty="hard">Difficile</button>
-        <button class="difficulty" data-difficulty="impossible">Impossible</button>
-    </div>
-    <section class="memory-game">
-
-    <!-- Boucle pour afficher chaque carte de mémoire -->
-        <?php foreach ($game_logos as $nom => $logo): ?>
-            <div class="memory-card" data-framework="<?=$nom;?>">
-
-                <!-- Image de face de la carte (visible quand la carte est retournée) -->
-                <img class="front-face" src="<?=$logo;?>" alt="<?=$nom;?>"/>
-
-                <!-- Image de dos de la carte (visible quand la carte n'est pas retournée) -->
-                <img class="back-face" src="/assets/img/rugbyCdm/backgroundCard.png" alt="Rugby badge"/>
+        <aside>
+            <div id="difficultyModal" class="modal">
+                <h2>Choisissez votre difficulté</h2>
+                <button class="difficulty" data-difficulty="easy">Facile</button>
+                <button class="difficulty" data-difficulty="medium">Moyen</button>
+                <button class="difficulty" data-difficulty="hard">Difficile</button>
+                <button class="difficulty" data-difficulty="impossible">Impossible</button>
             </div>
-            <div class="memory-card" data-framework="<?=$nom;?>">
-                <img class="front-face" src="<?=$logo;?>" alt="<?=$nom;?>"/>
-                <img class="back-face" src="/assets/img/rugbyCdm/backgroundCard.png" alt="Rugby badge"/>
-            </div>
-        <?php endforeach;?>
-    </section>
+        </aside>
 
-    <!-- Message de félicitations affiché à la fin du jeu -->
         <aside id="congratsModal" style="display: none">
-            <h2 id="congratsMessage">
-                Félicitations ! Vous avez gagné !
-            </h2>
-
-            <!-- Afficheurs pour montrer le nombre de mouvements et le temps final -->
+            <h2 id="congratsMessage">Félicitations ! Vous avez gagné !</h2>
             <p id="finalMoveCounter">Mouvements : 0</p>
             <p id="finalTimer">Temps écoulé : 0s</p>
-
-            <!-- Bouton pour rejouer -->
             <button id="resetButton">Rejouer</button>
         </aside>
+
+        <section class="memory-game" id="cardsContainer">
+        </section>
     </main>
 
     <script src="/assets/script/script.js"></script>
 </body>
+
 </html>
